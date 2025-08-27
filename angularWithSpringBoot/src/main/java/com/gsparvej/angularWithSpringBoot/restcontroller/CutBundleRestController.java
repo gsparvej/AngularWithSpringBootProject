028313c91fd594b980eae8e936ea7d321c3c04f7
gsparvej <parvejmd4469@gmail.com>
2025-08-27 19:11:26 +0600

    added

---
+++ angularWithSpringBoot/src/main/java/com/gsparvej/angularWithSpringBoot/restcontroller/CutBundleRestController.java
@@ -0,0 +1,34 @@
+package com.gsparvej.angularWithSpringBoot.restcontroller;
+
+import com.gsparvej.angularWithSpringBoot.entity.CutBundle;
+import com.gsparvej.angularWithSpringBoot.entity.CuttingPlan;
+import com.gsparvej.angularWithSpringBoot.service.CutBundleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cutBundle")
+@CrossOrigin("*")
+public class CutBundleRestController {
+
+    @Autowired
+    private CutBundleService cutBundleService;
+
+    @GetMapping("")
+    public List<CutBundle> getAllCutBundle() {
+        return cutBundleService.getAllCutBundles();
+    }
+    @PostMapping("")
+    public ResponseEntity<CutBundle> createCutBundle(
+            @RequestBody CutBundle cutBundle
+
+    ) {
+
+        CutBundle saved = cutBundleService.saveOrUpdate(cutBundle);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+}
--- angularWithSpringBoot/src/main/java/com/gsparvej/angularWithSpringBoot/service/CutBundleService.java
+++ angularWithSpringBoot/src/main/java/com/gsparvej/angularWithSpringBoot/service/CutBundleService.java
@@ -1,9 +1,16 @@
 package com.gsparvej.angularWithSpringBoot.service;
 
+import com.gsparvej.angularWithSpringBoot.entity.CutBundle;
+import com.gsparvej.angularWithSpringBoot.entity.CuttingPlan;
+import com.gsparvej.angularWithSpringBoot.entity.ProductionOrder;
+import com.gsparvej.angularWithSpringBoot.entity.UOM;
 import com.gsparvej.angularWithSpringBoot.repository.ICutBundleRepo;
 import com.gsparvej.angularWithSpringBoot.repository.ICuttingPlanRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CutBundleService {
@@ -14,5 +21,24 @@ public class CutBundleService {
     @Autowired
     private ICuttingPlanRepo cuttingPlanRepo;
 
-    
+    public List<CutBundle> getAllCutBundles() {
+        return cutBundleRepo.findAll();
+    }
+    public Optional<CutBundle> getCutBundleById (Integer id) {
+        return cutBundleRepo.findById(id);
+    }
+    public CutBundle saveOrUpdate(CutBundle cutBundle) {
+        CuttingPlan cuttingPlan = cuttingPlanRepo.findById(cutBundle.getCuttingPlan().getId())
+                .orElseThrow(() -> new RuntimeException("Cutting Plan not found with id: " + cutBundle.getCuttingPlan().getId()));
+
+
+        cutBundle.setCuttingPlan(cuttingPlan);
+
+
+        return cutBundleRepo.save(cutBundle);
+    }
+
+    public void deleteById(Integer id) {
+        cuttingPlanRepo.deleteById(id);
+    }
 }
